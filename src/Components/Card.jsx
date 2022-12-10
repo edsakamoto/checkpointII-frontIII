7b6d0567d15efc196b0a7db6ee4a366891cd6bba
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-12-09 22:41:45 -0300

    adicionando dados da api no Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = (props) => {
 
   return (
     <>
@@ -15,8 +15,9 @@ const Card = () => {
         <div className={`card-body ${styles.CardBody}`}>
           {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
           que vem da API */}
-          <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
+          <a href={`/dentist/MatriculaDoDentista`}>           
+            <h5 className={`card-title ${styles.title}`}>{props.data.nome + ' ' + props.data.sobrenome}</h5>
+
           </a>
         </div>
       </div>
